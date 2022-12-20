16f32b82ebd0bca5934694681a87270cb547a983
Tsion Moges <tsionmoges25@gmail.com>
2022-12-20 10:30:53 +0300

    installing config files

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,12 @@
+function taskFirst(){
+    const x = 'bye bye';
+    return x;
+}
+
+function taskNext(){
+    let y = 'good bye';
+    return y;
+}
+
+console.log( taskFirst());
+console.log( taskNext());
