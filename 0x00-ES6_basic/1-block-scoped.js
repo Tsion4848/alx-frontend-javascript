059f4b8392fd1ecb8ec35190ff40610aa742e329
Tsion Moges <tsionmoges25@gmail.com>
2022-12-20 10:58:55 +0300

    done

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,1 +1,13 @@
-
+var task;
+var task2;
+export default function taskBlock(trueOrFalse) {
+    task = false;
+    task2 = true;
+  
+    if (trueOrFalse) {
+      task = true;
+      task2 = false;
+    }
+  
+    return [task, task2];
+  }
