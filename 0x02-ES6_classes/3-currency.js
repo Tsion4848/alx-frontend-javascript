a9fe87e2b647252f91f670e051db6f5979b64581
Tsion Moges <tsionmoges25@gmail.com>
2022-12-23 10:28:03 +0300

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,5 +1,5 @@
 export default class Currency {
-  constructor(code, name){
+  constructor(code, name) {
     if (typeof code !== 'string') {
       throw new TypeError('Code must be a string');
     } else if (typeof name !== 'string') {
@@ -9,26 +9,30 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
-    
-  get code(){
-    retrun this._code;
-  }
-  get name(){
+
+  get name() {
     return this._name;
   }
-  set code(code){
-    if (typeof code !== 'string') {
-      throw new TypeError('code must be a string');
-    }
-    this._code = code;
-  }    
-  set name(name){
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
-  displayFullCurrency(){
-    return `${this._name} (${this._code})`
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }
