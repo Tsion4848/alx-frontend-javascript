3b0b0683cdc84fc61c3dc4a1bd1b663fe28844e4
Tsion Moges <tsionmoges25@gmail.com>
2022-12-23 15:28:15 +0300

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,7 +3,7 @@ import ClassRoom from './0-classroom';
 export default function initializeRooms() {
   let sizes = [new ClassRoom(19),
   new ClassRoom(20),
-  new ClassRoom(34)
+  new ClassRoom(34),
   ];
   return sizes;
 }
