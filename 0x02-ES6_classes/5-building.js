d15b78f8d7d776e433e40f39b0e0ca93416c1d59
Tsion Moges <tsionmoges25@gmail.com>
2022-12-23 11:08:43 +0300

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,14 @@
-export default class Building{
-  constructor(sqft = 0){
-    if (typeof sqft !== 'number'){
-      throw new TypeError('sqft must be a number')
+export default class Building {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+
     this._sqft = sqft;
   }
-  get sqft(){
+
+  get sqft() {
     return this._sqft;
-  }
-  evacuationWarningMessage(){
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
