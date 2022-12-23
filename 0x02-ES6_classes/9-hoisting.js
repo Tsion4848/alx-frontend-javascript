fcfe0d6fe5bf54c08f0bb760dd13ed9e90261ec5
Tsion Moges <tsionmoges25@gmail.com>
2022-12-23 14:54:45 +0300

    Update 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -35,7 +35,6 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
@@ -44,4 +43,4 @@ const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
 
-export default listOfStudents
+export default listOfStudents;
