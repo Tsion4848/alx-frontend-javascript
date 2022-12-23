f84620a10b66ca486298610c829713cefed57977
Tsion Moges <tsionmoges25@gmail.com>
2022-12-23 10:04:15 +0300

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,14 +1,15 @@
 export default class HolbertonCourse{
   constructor(name, length, students){
     if (typeof name != 'string'){
-      throw new TypeError('name should be a string');
+      throw new TypeError('name must be a string');
     }
     if (typeof length != 'number'){
-      throw new TypeError('length should be a number');
+      throw new TypeError('length must be a number');
     }
     if (!Array.isArray(students)){
-      throw new TypeError('students must be an array')
+      throw new TypeError('students must be an array of strings')
     }
+    
     this._name = name;
     this._length = length;
     this._students = students;
@@ -26,19 +27,19 @@ export default class HolbertonCourse{
   
   set name(name){
     if (typeof name != 'string'){
-      throw new TypeError('name should be a string');
+      throw new TypeError('name must be a string');
     }
     this._name = name;
   }
   set length(length){
     if (typeof length != 'number'){
-      throw new TypeError('length should be a number');
+      throw new TypeError('length must be a number');
     }
     this._length = length;
   }
   set students(students){
     if(!Array.isArray(students){
-       throw new TypeError('students should be an array');
+       throw new TypeError('students must be an array of strings');
     }
     this._students = students;
   }
